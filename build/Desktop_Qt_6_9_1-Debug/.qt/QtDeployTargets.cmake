aa2829cd89fc39edefef82820513d77bf4707b05
2023051604088zhanghuizhi <2439451174@qq.com>
2025-06-18 10:13:31 +0800

    Ignore build directory

--- build/Desktop_Qt_6_9_1-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appCHN-Chess_FILE /run/media/root/f6ad52dd-6a3e-40b7-9a1f-8323c318e456/qt/CHN-Chess/build/Desktop_Qt_6_9_1-Debug/appCHN-Chess)
-set(__QT_DEPLOY_TARGET_appCHN-Chess_TYPE EXECUTABLE)
